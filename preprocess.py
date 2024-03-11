be3abc41ca36f293f2664a8c5f8630a371636fee
kdrkdrkdr <kdrhacker1234@gmail.com>
2024-03-12 01:35:44 +0900

    add files

---
+++ preprocess.py
@@ -0,0 +1,27 @@
+import argparse
+import text
+from utils import load_filepaths_and_text
+# from text.j2k import japanese2korean
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--out_extension", default="cleaned")
+    parser.add_argument("--text_index", default=1, type=int)
+    parser.add_argument("--filelists", nargs="+", default=["filelists/kasumi_train.txt", "filelists/kasumi_val.txt"])
+    parser.add_argument("--text_cleaners", nargs="+", default=["jk_cleaners"])
+
+    args = parser.parse_args()
+
+
+    for filelist in args.filelists:
+        print("START:", filelist)
+        filepaths_and_text = load_filepaths_and_text(filelist)
+        for i in range(len(filepaths_and_text)):
+            original_text = '[JA]'+filepaths_and_text[i][args.text_index]+'[JA]'
+            # original_text = ('[KO]'+japanese2korean(original_text)+'[KO]').replace('[PREPROCESSED]', '')
+            cleaned_text = text._clean_text(original_text, args.text_cleaners)
+            filepaths_and_text[i][args.text_index] = cleaned_text
+
+        new_filelist = filelist + "." + args.out_extension
+        with open(new_filelist, "w", encoding="utf-8") as f:
+            f.writelines(["|".join(x) + "\n" for x in filepaths_and_text])
